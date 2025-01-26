48c1d7963ad0d1da96695aedcfeeefd162fdb277
Bryan Atiencia <baatiencia@uce.edu.ec>
2025-01-26 18:53:01 -0500

    feat: :memo: New Init SQL

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,15 @@
 CREATE DATABASE IF NOT EXISTS db_aerolinea;
+USE db_aerolinea;
 
-CREATE TABLE aerolineas (
-    ID INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS aerolineas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
     numero_aviones INT NOT NULL
 );
+
+INSERT INTO aerolineas (nombre, numero_aviones) VALUES 
+('BryanLine', 10),
+('PaoFlight', 25),
+('JFJC', 18),
+('AeroMundo', 30),
+('AeroEcuador', 50);
